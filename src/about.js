f9dcbd88cf403341407d2e794f53e8c10e7a0e24
Miguel Martín López <mmartlopez@gmail.com>
2018-09-18 22:31:36 +0200

    About

--- src/about.js
+++ src/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-export default class A extends React.Component {
+export default class About extends React.Component {
     render() {
         return <p>A! <NavLink to="/b">B</NavLink>
         <button type="button" className="btn btn-primary btn-lg">Large button</button>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import A from './a';
+import About from './about';
 import B from './b';
 import {Layout} from './layout';
 import * as Popper from 'popper.js';
 import * as Bootstrap from 'bootstrap';
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Router>
     <div>
         <Layout>
-            <Route exact path="/" component={A} />
-            <Route exact path='/a' component={A} />
+            <Route exact path="/" component={About} />
+            <Route exact path='/a' component={About} />
             <Route exact path='/b' component={B} />
         </Layout>
     </div>
