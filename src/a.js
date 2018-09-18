853e935fa68d80016f71c14dae604b87621960f4
Miguel Martín López <mmartlopez@gmail.com>
2018-09-18 21:58:43 +0200

    Layout

--- src/a.js
+++ src/a.js
@@ -3,7 +3,10 @@ import {NavLink} from 'react-router-dom';
 
 export default class A extends React.Component {
     render() {
-        return <p>A! <NavLink to="/b">B</NavLink></p>;
+        return <p>A! <NavLink to="/b">B</NavLink>
+        <button type="button" className="btn btn-primary btn-lg">Large button</button>
+<button type="button" className="btn btn-secondary btn-lg">Large button</button>
+        </p>;
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import A from './a';
 import B from './b';
+import {Layout} from './layout';
+import * as Popper from 'popper.js';
+import * as Bootstrap from 'bootstrap';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Router>
     <div>
-        <Route exact path="/" component={A} />
-        <Route exact path='/a' component={A} />
-        <Route exact path='/b' component={B} />
+        <Layout>
+            <Route exact path="/" component={A} />
+            <Route exact path='/a' component={A} />
+            <Route exact path='/b' component={B} />
+        </Layout>
     </div>
 </Router>, document.getElementById('app')
 );
