f923ddad1528b58865225823fc0e9eebfc2779ba
Miguel Martín López <mmartlopez@gmail.com>
2018-09-20 22:58:17 +0200

    icons

--- src/about.js
+++ src/about.js
@@ -3,16 +3,14 @@ import user from './img/user.png';
 
 export default class About extends React.Component {
     render() {
-        return <div className="text-center"><h1><br/><br/><br/><br/>About</h1><div className="slide-wrapper-inner">
-										
-        <img src={user} width="200" className="logo rounded-circle mb-4"/>
-                                    
-        <h1>Miguel Martín</h1>
-        <p className="subtitle mt-3">123 Main Street, Barcelona. &nbsp;|&nbsp; <a href="#">mmartlopez@email.com</a> &nbsp;|&nbsp; (123) 456 7890
-                                    
-        </p><p className="lead">Highly creative and multitalented Graphic Designer with extensive experience in multimedia, marketing and print design. Exceptional collaborative and interpersonal skills; dynamic team player with well-developed written and verbal communication abilities.</p>
-
-    </div></div>;
+        return <div className="">
+            <h1>About</h1>
+            <div className="slide-wrapper-inner text-center">
+                <img src={user} width="200" className="logo rounded-circle mb-4" />
+                <h1>Miguel Martín</h1>
+                <p className="subtitle mt-3">123 Main Street, Barcelona. &nbsp;|&nbsp; <a href="#">mmartlopez@email.com</a> &nbsp;|&nbsp; (123) 456 7890</p>
+                <p className="lead">Highly creative and multitalented Graphic Designer with extensive experience in multimedia, marketing and print design. Exceptional collaborative and interpersonal skills; dynamic team player with well-developed written and verbal communication abilities.</p>
+            </div>
+        </div>;
     }
-
 }
--- src/home.js
+++ src/home.js
@@ -1,47 +1,45 @@
 import React from 'react';
-//import doneAll from '@material-ui/icons/';
+import Fingerprint from '@material-ui/icons/Fingerprint';
+const FontAwesome = require('react-fontawesome');
 
 export default class Home extends React.Component {
     render() {
         return <div>
-            <section class="divided clearfix">
-                <div class="container">
-                    <div class="row py-5">
-                        <div class="col col-12 col-sm-12 col-md-6 p-lg-5">
-                            <div class="p-5">
-                                <h5 class="h5 text-muted">Bootstrap </h5>
-                                <h1 class="h1 mb-5">Spacing </h1>
+            <section className="divided clearfix">
+                <div className="container">
+                    <div className="row py-5">
+                        <div className="col col-12 col-sm-12 col-md-6 p-lg-5">
+                            <div className="p-5">
+                                <h5 className="h5 text-muted">Bootstrap </h5>
+                                <h1 className="h1 mb-5">Spacing </h1>
                                 <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance.</p>
                             </div>
                         </div>
-                        <div class="versus d-sm-none">+</div>
-                        <div class="col col-12 col-sm-12 col-md-6 p-lg-5">
-                            <div class="p-5">
-                                <h5 class="h5 text-muted">Bootstrap</h5>
-                                <h1 class="h1 mb-5">Spacing </h1>
+                        <div className="versus d-xs-none d-sm-block">+</div>
+                        <div className="col col-12 col-sm-12 col-md-6 p-lg-5">
+                            <div className="p-5">
+                                <h5 className="h5 text-muted">Bootstrap</h5>
+                                <h1 className="h1 mb-5">Spacing </h1>
                                 <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance.</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <section class="border-bottom">
-                <div class="container">
-                    <div class="row text-center py-5">
-                        <div class="col col-12 border-bottom border-sm-bottom border-md-bottom-0 border-md-right col-sm-12 col-md-6 p-5"> <img class="m-auto" width="40px" src="https://semantic-ui.com/images/icons/mobile.png" />
-                            <h2 class="h3 mt-3 mb-5 text-muted">Bootstrap Spacing</h2>
+            <section className="border-bottom">
+                <div className="container">
+                    <div className="row text-center py-5">
+                        <div className="col col-12 border-bottom border-sm-bottom border-md-bottom-0 border-md-right col-sm-12 col-md-6 p-5"> <img className="m-auto" width="40px" src="https://semantic-ui.com/images/icons/mobile.png" />
+                            <h2 className="h3 mt-3 mb-5 text-muted">Bootstrap Spacing</h2>
                             <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance. </p>
                         </div>
-                        <div class="col col-12 col-sm-12 col-md-6 p-5"> 
-                            <h2 class="h3 mt-3 mb-5 text-muted">Bootstrap Spacing</h2>
+                        <div className="col col-12 col-sm-12 col-md-6 p-5">    <FontAwesome name='rocket' /> <Fingerprint className="display-3" />
+                            <h2 className="h3 mt-3 mb-5 text-muted">Bootstrap Spacing</h2>
                             <p>Bootstrap includes a wide range of shorthand responsive margin and padding utility classes to modify an element’s appearance. </p>
                         </div>
                     </div>
                 </div>
             </section>
-
-
-            <a target="_blank" href="https://s.bootsnipp.com/iframe/1eZNd"><small>Go To Full View<small></small></small></a>
         </div>;
     }
 }
